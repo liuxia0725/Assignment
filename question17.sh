04e1f1eb0df80ac3c5b122379ee04730595ef327
Chris Liu <chrisliu961127@gmail.com>
2024-02-10 00:32:31 +1030

    qw

--- question17.sh
+++ question17.sh
@@ -4,13 +4,13 @@ git branch branch2
 mv dir1/dir2/foo dir1/
 touch dir3/bar_copy
 git add .
-git commit
+git commit -m "main change"
 git checkout branch1
 touch newfile1
 mv dir1/dir2/foo dir1/
 rm -rf dir1/dir2
 git add .
-git commit
+git commit -m "branch1 change"
 git checkout branch2
 cd dir1/dir2
 mv foo foo_modified
@@ -18,8 +18,8 @@ cd ../..
 mv -f dir3/ dir1/
 mv dir1/dir3/bar dir1/dir3/newfile2
 git add .
-git commit
+git commit -m "branch2 change"
 cd ..
 git add
-git commit
+git commit -m "question17 change"
 git push
