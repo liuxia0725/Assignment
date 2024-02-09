8d41cd510742a59f9f263418e2d03645f34104f6
Chris Liu <chrisliu961127@gmail.com>
2024-02-10 01:25:45 +1030

    change18

--- question18.sh
+++ question18.sh
@@ -2,11 +2,18 @@ cd git-practice-04
 
 git branch -a
 git checkout main
-git pull origin main  # Ensure main is up to date
-git branch --list 'ready*' | xargs -I {} git merge {}
-
-git branch --list 'ready*' | xargs -I {} git branch -d {}
-
-git branch --list 'update*' | xargs -I {} git checkout {}
+git pull origin main  
+git checkout -b ready1 origin/ready1
+git checkout -b ready2 origin/ready2
+git checkout -b ready3 origin/ready3
+git merge ready1 -m "ready1"
+git merge ready2 -m "ready2"
+git merge ready3 -m "ready3"
+git branch -d ready1
+git branch -d ready2
+git branch -d ready3
+git checkout -b update2 origin/update2
+git pull origin main:update1
+git pull origin main:update2
 git pull origin main
 
