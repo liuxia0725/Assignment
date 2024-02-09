21d2013c52c1f295ba704d15d3afa44c70954c10
Chris Liu <chrisliu961127@gmail.com>
2024-02-09 23:27:19 +1030

    question17

--- question17.sh
+++ question17.sh
@@ -1,19 +1,24 @@
 cd git-practice-03
 git branch branch1
 git branch branch2
+mv dir1/dir2/foo dir1/
 touch dir3/bar_copy
 git add .
 git commit
+git checkout branch1
 touch newfile1
 mv dir1/dir2/foo dir1/
 rm -rf dir1/dir2
 git add .
 git commit
+git checkout branch2
 cd dir1/dir2
 mv foo foo_modified
 cd ../..
 mv -f dir3/ dir1/
-mv dir1/dir3/bar dir1/dir3/newfile2
+mv dir1/dir3/bar dir1/newfile2
+git add .
+git commit
 cd ..
 git add
 git commit
